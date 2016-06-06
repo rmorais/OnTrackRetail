1b665913bbba24685e46fbb1b4d05c69ad2ec059
Rui Morais <rui.morais@gmail.com>
2016-06-06 11:47:58 +0100

    add partial solution to the futures exercise

---
+++ src/main/scala/com/ontrackretail/Futures.scala
@@ -0,0 +1,68 @@
+package com.ontrackretail
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
+trait Futures {
+
+  def f1 = Future {
+    Thread.sleep(200)
+    println(s"Future 1")
+  }
+
+  def f2 = Future {
+    Thread.sleep(100)
+    println(s"Future 2")
+  }
+
+  def f3 = Future {
+    Thread.sleep(50)
+    println(s"Future 3")
+  }
+
+  def f4 = Future {
+    Thread.sleep(10)
+    println(s"Future 4")
+  }
+}
+
+object IndependentFutures extends Futures {
+  def main(args: Array[String]): Unit = {
+    f1;f2;f3;f4
+
+    Thread.sleep(500)
+    println("finished independent futures")
+  }
+}
+
+object ChainedFutures extends Futures {
+  def main(args: Array[String]): Unit = {
+    for {
+      r1 <- f1
+      r2 <- f2
+      r3 <- f3
+      r4 <- f4
+    } yield {
+      println("finished chained futures")
+    }
+
+    Thread.sleep(500)
+  }
+}
+
+object DependentFutures extends Futures {
+  def main(args: Array[String]): Unit = {
+
+    //TODO FIXIT
+    val dep1 = f1.flatMap { _ => f2;f3}
+
+    for {
+      r1 <- dep1
+      r1 <- f4
+    } yield println("finished dependent futures")
+
+    Thread.sleep(500)
+
+  }
+}
---
+++ src/main/scala/com/ontrackretail/Increment.scala
@@ -0,0 +1,8 @@
+package com.ontrackretail
+
+/**
+  * Created by rmorais on 06/06/2016.
+  */
+class Increment {
+
+}
