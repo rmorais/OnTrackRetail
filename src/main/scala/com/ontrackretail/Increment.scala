1fd3c0c685187408e0d087221ab803539e4ee95f
Rui Morais <rui.morais@gmail.com>
2016-06-06 11:52:05 +0100

    increment of a single digit

--- src/main/scala/com/ontrackretail/Increment.scala
+++ src/main/scala/com/ontrackretail/Increment.scala
@@ -5,7 +5,12 @@ import scala.collection.immutable.Nil
 object Increment extends App {
 
 
-  def inc(number: Seq[Int]) = Nil
+  def inc(number: Seq[Int]): Seq[Int] = number match {
+    case Nil => Nil
+    case x :: xs => (x + 1) +: inc(xs)
+  }
 
-  assert(inc(Nil)==Nil, "Failed to increment empty list")
+  assert(inc(Nil) == Nil, "Failed to increment empty list")
+  assert(inc(Seq(0)) == Seq(1), "Failed to increment one digit")
+  assert(inc(Seq(8)) == Seq(9), "Failed to increment one digit")
 }
